5fea6c6e552914e75c3ceb08e92a43daebe00df4
edisonwong <edisonwong520@gmail.com>
2019-05-08 14:28:24 +0800

    fix pwd refind

--- Demo_Clock/Me/FindPasswordViewController.swift
+++ Demo_Clock/Me/FindPasswordViewController.swift
@@ -10,24 +10,21 @@ import UIKit
 
 class FindPasswordViewController: UIViewController, UITextFieldDelegate {
     
-    @IBOutlet weak var scrollView: UIScrollView!
+    
     
     @IBOutlet weak var quitButton: UIButton!
-    @IBOutlet var textfieldsCollection: [UITextField]!
     
-    @IBOutlet weak var realName: UITextField! // as First Field that contain name in View
+    
     @IBOutlet weak var userName: UITextField! //as Second Field that contain username in View
-    @IBOutlet weak var emailAddr: UITextField! //as Third Field that contain email address in View
-    @IBOutlet weak var password: UITextField! //as Fourth Field that contain password in View
-    @IBOutlet weak var passwordConfirmation: UITextField! //as Fifth Field that contain password-confirm in View
+    
+    @IBOutlet weak var oldpassword: UITextField! //as Fourth Field that contain password in View
+    @IBOutlet weak var newpassword: UITextField! //as Fifth Field that contain password-confirm in View
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.textfieldsCollection.forEach { (textfld) in
-            textfld.delegate = self
-        }
+        
         
     }
 
@@ -39,40 +36,46 @@ class FindPasswordViewController: UIViewController, UITextFieldDelegate {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(false)
     }
-    @IBAction func signupTapped(_ sender: UIButton) {
-        if !check_pwd(){
-            return
-        }
-        if !check_email(){
-            let alert = UIAlertView(title: "提醒", message: "邮箱格式不对" ,delegate: nil, cancelButtonTitle: "OK")
+    @IBAction func confirmTapped(_ sender: UIButton) {
+        let md5_str = newpassword.text?.md5() ?? ""
+        let id = check_pwd()
+        if id != -1 {
+            let sql = "UPDATE UserDB SET password='\(md5_str)' WHERE id=\(id);"
+            if DBManager.shareManager().execute_sql(sql: sql){
+                NSLog("change pwd success")
+                let alert = UIAlertView(title: "提醒", message: "密码更改成功！" ,delegate: nil, cancelButtonTitle: "OK")
+                alert.show()
+                dismiss(animated: true)
+                
+            }else{
+                NSLog("change pwd failed")
+            }
+        }else{
+            let alert = UIAlertView(title: "提醒", message: "信息错误，无法更改" ,delegate: nil, cancelButtonTitle: "OK")
             alert.show()
             return
         }
-        let user = UserItem()
-        user.name = userName.text!
-        user.email = emailAddr.text!
-        user.realname = realName.text!
-        user.password = (password.text?.md5())!
-        DBManager.shareManager().insert_user_db(user: user)
-        dismiss(animated: true)
-        
     }
     
-    func check_email()->Bool{
-        let email = emailAddr.text
-        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
-        let emailTest:NSPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
-        
-        return emailTest.evaluate(with: email)
-    }
     
-    func check_pwd()->Bool{
-        if password.text != passwordConfirmation.text{
-            let alert = UIAlertView(title: "提醒", message: "两次密码不一样，请重新输入" ,delegate: nil, cancelButtonTitle: "OK")
-            alert.show()
-            return false
+    //find pwd in db
+    func check_pwd()->Int{
+        
+        let md5_str = oldpassword.text?.md5() ?? ""
+        var sql = "SELECT id FROM UserDB Where username='\(userName.text ?? "")' AND password='\(md5_str)';"
+        var result = DBManager.shareManager().get_single_col(sql: sql)
+        if result == ""{
+            sql = "SELECT id FROM UserDB Where email='\(userName.text ?? "")' AND password='\(md5_str)';"
+            result = DBManager.shareManager().get_single_col(sql: sql)
+            if result != ""{
+                return Int(result)!
+            }
+        }else{
+            return Int(result)!
         }
-        return true
+        
+        
+        return -1
     }
     
     @IBAction func quitTapped(_ sender: Any) {
@@ -85,13 +88,7 @@ class FindPasswordViewController: UIViewController, UITextFieldDelegate {
         return true
     }
     
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
-    }
     
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        scrollView.setContentOffset(CGPoint(x: 0, y: 110), animated: true)
-    }
     
 
 }
